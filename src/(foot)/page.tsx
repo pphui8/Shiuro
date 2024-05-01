25847d05d89fa410aa4007a9c79b111595d40de5
pphui8 <1216595344@qq.com>
2024-05-01 16:28:56 +0900

    update

--- src/(foot)/page.tsx
+++ src/(foot)/page.tsx
@@ -6,12 +6,27 @@ export default function Foot() {
       <div className={styles.timelineContainer}>
         <div className={styles.titile}>Timeline</div>
         <ul className={styles.timelines}>
-          <li className={styles.timelineItem}>now developing</li>
-          <li className={styles.timelineItem}>@icon 2024/1/21 Main website framework is completed</li>
-          <li className={styles.timelineItem}>@icon 2024/1/2 Successfully deployed via Github Action</li>
-          <li className={styles.timelineItem}> @icon 2023/12/24 first commit</li>
+          <li className={styles.timelineItem}><ListContent time="" description="" /></li>
+          <li className={styles.timelineItem}><ListContent time="2024/1/21" description="Main website framework is completed" /></li>
+          <li className={styles.timelineItem}><ListContent time="2024/1/2" description="Successfully deployed via Github Action" /></li>
+          <li className={styles.timelineItem}><ListContent time="2023/12/24" description="first commit"/></li>
         </ul>
       </div>
     </div>
+  )
+}
+
+type Props = {
+  time: string
+  description: string
+}
+
+const ListContent = (props: Props) => {
+  return (
+    <>
+      <div className={styles.iconContainer}><i className="mgc_bling_line" /></div>
+      <div className={styles.time}>{props.time}</div>
+      <div className={styles.description}>{props.description}</div>
+    </>
   )
 }
