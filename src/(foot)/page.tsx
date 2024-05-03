47c1dfc7ccb3b32500fd784c0046d37ecb361e1f
pphui8 <1216595344@qq.com>
2024-05-03 20:49:59 +0900

    update

--- src/(foot)/page.tsx
+++ src/(foot)/page.tsx
@@ -6,7 +6,7 @@ export default function Foot() {
       <div className={styles.timelineContainer}>
         <div className={styles.titile}>Timeline</div>
         <ul className={styles.timelines}>
-          <li className={styles.timelineItem}><ListContent time="" description="" /></li>
+          <li className={styles.timelineItem}><ListContent time="~now" description="developing" /></li>
           <li className={styles.timelineItem}><ListContent time="2024/1/21" description="Main website framework is completed" /></li>
           <li className={styles.timelineItem}><ListContent time="2024/1/2" description="Successfully deployed via Github Action" /></li>
           <li className={styles.timelineItem}><ListContent time="2023/12/24" description="first commit"/></li>
@@ -24,7 +24,7 @@ type Props = {
 const ListContent = (props: Props) => {
   return (
     <>
-      <div className={styles.iconContainer}><i className="mgc_bling_line" /></div>
+      <div className={styles.iconContainer}><i className="mgc_bling_line"/></div>
       <div className={styles.time}>{props.time}</div>
       <div className={styles.description}>{props.description}</div>
     </>
